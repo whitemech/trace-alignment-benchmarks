856f90c62e2c4f9a2ec999653c16815020623b9e
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2023-03-01 14:00:55 +0100

    update

--- benchmark/experiments/server-exp.sh
+++ benchmark/experiments/server-exp.sh
@@ -7,3 +7,11 @@ echo "Output directory: ${OUTPUT_DIR}"
 python ./benchmark/experiments/experiment-1.py --constraints 10 -i 3 -i 4 -i 6 -l '1-50' -l '51-100' -l '101-150' -l '151-200' -t tral-g-fd-blind -t tral-g-fd-hmax -t tral-gc-fd-blind -t tral-gc-fd-hmax -t tral-gs-fd-blind -t tral-gs-fd-hmax -t tral-gcs-fd-blind -t tral-gcs-fd-hmax -t tral-strips-fd-blind  -t tral-strips-fd-hmax -t tral-strips-symba --output-dir ${OUTPUT_DIR}/server-10 --timeout 3600 --stop-on-timeout
 python ./benchmark/experiments/experiment-1.py --constraints 15 -i 3 -i 4 -i 6 -l '1-50' -l '51-100' -l '101-150' -l '151-200' -t tral-g-fd-blind -t tral-g-fd-hmax -t tral-gc-fd-blind -t tral-gc-fd-hmax -t tral-gs-fd-blind -t tral-gs-fd-hmax -t tral-gcs-fd-blind -t tral-gcs-fd-hmax -t tral-strips-fd-blind  -t tral-strips-fd-hmax -t tral-strips-symba --output-dir ${OUTPUT_DIR}/server-15 --timeout 3600 --stop-on-timeout
 python ./benchmark/experiments/experiment-1.py --constraints 20 -i 3 -i 4 -i 6 -l '1-50' -l '51-100' -l '101-150' -l '151-200' -t tral-g-fd-blind -t tral-g-fd-hmax -t tral-gc-fd-blind -t tral-gc-fd-hmax -t tral-gs-fd-blind -t tral-gs-fd-hmax -t tral-gcs-fd-blind -t tral-gcs-fd-hmax -t tral-strips-fd-blind  -t tral-strips-fd-hmax -t tral-strips-symba --output-dir ${OUTPUT_DIR}/server-20 --timeout 3600 --stop-on-timeout
+
+
+python ./benchmark/experiments/experiment-1.py --constraints 10 -i 3 -i 4 -i 6 -l '1-50' -l '51-100' -l '101-150' -l '151-200' -t tral-gs-fd-blind -t tral-gs-fd-hmax --output-dir ${OUTPUT_DIR}/server-10 --timeout 3600 --stop-on-timeout
+python ./benchmark/experiments/experiment-1.py --constraints 15 -i 3 -i 4 -i 6 -l '1-50' -l '51-100' -l '101-150' -l '151-200' -t tral-gs-fd-blind -t tral-gs-fd-hmax --output-dir ${OUTPUT_DIR}/server-15 --timeout 3600 --stop-on-timeout
+python ./benchmark/experiments/experiment-1.py --constraints 20 -i 3 -i 4 -i 6 -l '1-50' -l '51-100' -l '101-150' -l '151-200' -t tral-gs-fd-blind -t tral-gs-fd-hmax --output-dir ${OUTPUT_DIR}/server-20 --timeout 3600 --stop-on-timeout
+python ./benchmark/experiments/experiment-2.py -l 50 -l 75 -l 100 -l 128 -t tral-gs-fd-blind -t tral-gs-fd-hmax --output-dir ${OUTPUT_DIR}/server-exp2 --timeout 7200
+
+
