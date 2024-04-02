f85c68a05f2ebaf5f8667b10f2a8222c3753a15e
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2024-04-02 08:21:25 +0200

    add script for stats on noise

--- benchmark/plotting/compute_noise.py
+++ benchmark/plotting/compute_noise.py
@@ -1,8 +1,7 @@
-"""Compute percentage of noisy traces on logs."""
+"""Compute stats on noisy traces on logs."""
 from pathlib import Path
 
 import click
-import pandas as pd
 
 from benchmark.utils.base import try_to_get_all_float
 
@@ -18,23 +17,50 @@ def main(directory: str):
     """Compute percentage of noisy traces in input log traces."""
     number_of_constraints = ["10", "15", "20"]
     inverted_constraints = ["3", "4", "6"]
-    result = {k: {k: [] for k in inverted_constraints} for k in number_of_constraints}
+    logs = ["1", "2", "3", "4"]
+    percentages_of_noisy_traces_per_log = {
+        k: {k: [] for k in inverted_constraints} for k in number_of_constraints
+    }
+    percentages_of_noise_in_all_traces_per_log = {
+        k: {k: {k: [] for k in logs} for k in inverted_constraints}
+        for k in number_of_constraints
+    }
 
     for file in sorted(Path(directory).glob("*.txt")):
-        num_constraint, inverted, _ = Path(file).stem.split("-")
+        print(f"reading file {str(file)}")
+        num_constraint, inverted, log = Path(file).stem.split("-")
 
         with open(file, "r") as f:
             content = f.read()
             plan_costs = try_to_get_all_float("Plan cost: (.*)", content)
-            assert len(plan_costs) == 100, "Wrong number of plan costs"
-            noisy_traces = [x for x in plan_costs if x > 0]
-            assert len(noisy_traces) <= len(plan_costs), "Impossible number of noisy traces"
-            noisy_traces_percentage = len(noisy_traces) / len(plan_costs)
-        result[num_constraint][inverted].append(noisy_traces_percentage * 100)
+            plan_lengths = try_to_get_all_float("Plan length: (.*) step\(s\).", content)
+            assert (
+                len(plan_costs) == len(plan_lengths) == 100
+            ), "Wrong number of plan costs and plan length."
 
-    df = pd.DataFrame.from_dict({f"{i}-{j}": result[i][j] for i in result.keys() for j in result[i].keys()},
-                                orient="index")
-    df.transpose().to_csv("noisy_traces.csv", index=False, float_format='%.0f')
+        # compute percentage of noisy traces per log
+        noisy_traces = [single_cost for single_cost in plan_costs if single_cost > 0]
+        assert len(noisy_traces) <= len(plan_costs), "Impossible number of noisy traces"
+        percentage_of_noisy_traces_per_log = len(noisy_traces) / len(plan_costs)
+        percentages_of_noisy_traces_per_log[num_constraint][inverted].append(
+            percentage_of_noisy_traces_per_log * 100
+        )
+
+        for plan_cost, plan_length in zip(plan_costs, plan_lengths):
+            percentages_of_noise_in_all_traces_per_log[num_constraint][inverted][
+                log
+            ].append((plan_cost / plan_length) * 100)
+    print(percentages_of_noise_in_all_traces_per_log)
+
+    # df = pd.DataFrame.from_dict(
+    #     {
+    #         f"{i}-{j}": percentages_of_noisy_traces_per_log[i][j]
+    #         for i in percentages_of_noisy_traces_per_log.keys()
+    #         for j in percentages_of_noisy_traces_per_log[i].keys()
+    #     },
+    #     orient="index",
+    # )
+    # df.transpose().to_csv("noisy_traces.csv", index=False, float_format="%.0f")
 
 
 if __name__ == "__main__":
